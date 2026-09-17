281b080a80d0ac8678d137c059a9b008e07d112f
manuelem1984 <manuel@escanomejias.com>
2026-09-17 20:52:29 +0200

    Update default language and app version in const.py

--- custom_components/deepal_spain_dec/const.py
+++ custom_components/deepal_spain_dec/const.py
@@ -7,12 +7,19 @@ VERSION = "0.1.0-dev"
 # Spain configuration
 SPAIN_COUNTRY = "ES"
 SPAIN_DIAL_CODE = "34"
-DEFAULT_LANGUAGE = "es-ES"
-DEFAULT_APP_VERSION = "1.0.0"
+
+# Application configuration
+DEFAULT_LANGUAGE = "en_US"
+DEFAULT_APP_VERSION = "V1.11.0"
 
 # Deepal cloud endpoints
-BASE_URL = "https://eu-m.iov.changan.com"
+BASE_URL = "https://m.iov.changanauto.com.de"
 CA_BASE_URL = "https://ca-m.iov.changanauto.com.de"
 
 # HTTP configuration
 REQUEST_TIMEOUT = 30
+
+# Public key used to encrypt login values
+REQUEST_ENCRYPTION_PUBLIC_KEY = (
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkyhr43cBPTJ3jLiYsmbUwUp74cMJIOju5vqVzgtuK63Q99qV6iVT8wN5cXlyMtWI2mfOmhIao/fUN821im69MfOHsWXdqQEo5e9v654GPw+bju0pCphEPtD1I0VcyS34QkAu04urSun2U1q3Dr2OICLVWSnLa+01ioKxkaB0D209zXcls2eFQpvRAWm7xxVsoqzSwqp+neu5quOpn+eO/bW0TxcSQ8VZcDEUvadRTLSR0eOWgRuHIBiD2RGqPIPzKCm5A14q1qhxUZ8U0pmYe0Sx7eMy4RVe2iW7fnjc6pxTUMBkercSL26mevYouuCKqyie+LVQAtGa29RMl/lyiwIDAQAB"
+)
